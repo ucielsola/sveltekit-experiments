106ebe6e669b59414d8e587a40f18997f8c9e570
Uciel Sola <solauciel@gmail.com>
2022-01-24 04:34:31 -0300

    added transitions to slot change + simplified __layout structure

--- src/lib/components/sideNav.svelte
+++ src/lib/components/sideNav.svelte
@@ -1,15 +1,24 @@
-<nav>
-	<h2 class="subtitle">Markers</h2>
-	<li>
-		<a class="link" href="/scrollDirection">Scroll detection</a>
-	</li>
-	<li>
-		<a class="link" href="/reactiveness">Reactive Statements</a>
-	</li>
-</nav>
-
+<aside>
+	<nav>
+		<h2 class="subtitle">Markers</h2>
+		<li>
+			<a class="link" href="/scrollDirection">Scroll detection</a>
+		</li>
+		<li>
+			<a class="link" href="/reactiveness">Reactive Statements</a>
+		</li>
+	</nav>
+</aside>
 
 <style>
+	aside {
+		padding-inline: 1rem;
+		padding-block: 1rem;
+		min-height: calc(100vh - 6rem);
+		min-width: max-content;
+
+		border-right: 1px solid var(--highlight);
+	}
 
 	li {
 		padding-block: 0.5rem;
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,11 @@
+<script context="module">
+	export const load = async ({ url }) => ({
+		props: {
+			key: url
+		}
+	});
+</script>
+
 <script>
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
@@ -6,6 +14,8 @@
 	import SideNav from '$lib/components/sideNav.svelte';
 	import Spinner from '$lib/components/spinner.svelte';
 	import Header from '$lib/components/header.svelte';
+	import PageTransitions from '$lib/components/PageTransitions.svelte';
+	export let key;
 
 	let hide = true;
 
@@ -21,12 +31,12 @@
 <div class="container {browser ? 'show' : ''}">
 	<Header />
 	<div class="columns">
-		<aside>
-			<SideNav />
-		</aside>
+		<SideNav />
 
 		<main>
-			<slot />
+			<PageTransitions refresh={key}>
+				<slot />
+			</PageTransitions>
 		</main>
 	</div>
 </div>
@@ -37,28 +47,17 @@
 		display: flex;
 		flex-direction: column;
 	}
-
 	.container.show {
 		animation: fade-in 1.3s cubic-bezier(0.39, 0.575, 0.565, 1) 1s both;
 	}
-
 	.columns {
 		display: flex;
 		flex-direction: row;
 	}
-
-	aside {
-		padding-inline: 1rem;
-		padding-block: 1rem;
-		min-height: calc(100vh - 6rem);
-		border-right: 1px solid var(--highlight);
-	}
-
 	main {
 		padding-inline: 2rem;
 		padding-block: 1rem;
 	}
-
 	@keyframes fade-in {
 		0% {
 			opacity: 0;
