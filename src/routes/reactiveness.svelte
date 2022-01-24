ff2ca1dcd5b20bb0b156d3fdebe2339d209cb129
Uciel Sola <solauciel@gmail.com>
2022-01-24 04:25:02 -0300

    added mock pages

---
+++ src/routes/reactiveness.svelte
@@ -0,0 +1,8 @@
+<h1 class="title">Reactiveness - Reactive Statements</h1>
+
+<p>
+	<span class="highlight">Svelte</span> has a really usefull feature, the <a
+		href="https://svelte.dev/docs#component-format-script-3-$-marks-a-statement-as-reactive"
+		class="link">Reactive Statements</a
+	>
+</p>
---
+++ src/routes/scrollDirection.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let scrollY;
+	let scrollUp = false;
+	const showNav = () => {
+		setTimeout(() => {
+			console.log('stop');
+			return true;
+		}, 800);
+	};
+	let scrollPos = 0;
+
+	$: if (scrollY > scrollPos) {
+		console.log('start');
+		let oldScroll = 0;
+		scrollUp = oldScroll < scrollY;
+		showNav();
+	}
+</script>
+
+<svelte:window bind:scrollY />
+
+<svelte:head><title>Scroll Detection</title></svelte:head>
+
+<section>
+    <h1 class="title">Scroll detection</h1>
+</section>
