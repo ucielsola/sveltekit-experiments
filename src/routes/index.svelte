c270237262e484f6166726175856653d2227149a
Uciel Sola <solauciel@gmail.com>
2022-01-24 00:08:35 -0300

    addded landing and some global styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section>
+<h1 class="title">Hi! This is my place to test and break stuff with <a class="link" href="https://kit.svelte.dev">SvelteKit</a></h1>
+
+<h2><a id="to-portfolio" href="https://ucielsola.com">- Visit my portfolio -</a></h2>
+
+<h2 class="subtitle">The experiments:</h2>
+<nav>
+    <li>
+        <a class="link" href="/scrollDirection">Scroll detection</a>
+    </li>
+</nav>
+
+
+
+</section>
+
+
+<style>
+    * {
+        font-family: sans-serif;
+    }
+
+    section {
+        padding-inline: 1rem;
+    }
+
+    #to-portfolio {
+        display: block;
+        text-align: center;
+        font-size: 1.5rem;
+        color: aliceblue;
+}
+</style>
